49cff869e88acbcbcf0844256143f9e7b0441b78
pavdemesh <69710730+pavdemesh@users.noreply.github.com>
2020-08-15 12:53:59 +0300

    Add files via upload
    code from online tutorial from Giraffe Academy

---
+++ tutorial_GA.py
@@ -0,0 +1,54 @@
+# character_name = "Tom"
+# character_age = 50
+#
+# print("Character name is " + character_name + " .")
+# print("Character age is " + str(character_age) + " years.")
+
+# phrase = "Giraffe Academy"
+# # print(phrase.lower())
+# # print(phrase)
+# print(phrase.replace("Giraffe", "Elephant"))
+
+# from math import *
+#
+# print(sqrt(3.19999))
+
+# color, plural_noun, celebrity = input("Enter color, plural noun and celebrity: ").split('-')
+# print(f"Roses are {color}")
+# print(f"{plural_noun} are blue")
+# print(f"I love {celebrity}")
+
+# my_friends = ["Kevin", 2, False, "Mike", "Susie", 145, "John"]
+# print(my_friends)
+# print(f"Length of the array is {len(my_friends)}")
+# print(f"Accessing [1:4] means index 1, 2, 3, not including 4: {my_friends[1:4]}")
+# print(f"Elements [-4:-1] are {my_friends[-4:-1]}")
+# print("Since range stops before the second index in [], i cannot include last list element using negative range")
+# print("Because the program will stop before [-1] on index [-2]")
+# print(f"All elements starting with index [3] are: {my_friends[3:]}")
+# print(f"Or I can use [3:last_index+1], so [3:7] for a 7 array with actual last index 6: {my_friends[3:7]}")
+
+lucky_numbers = [15, 16, 4, 8, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+# friends.extend(lucky_numbers)
+# friends.append("Creed")
+# friends.append(lucky_numbers[0])
+# friends.insert(1,"Michael") # insert at specified index shifting everything else to the right
+# friends.remove("Kevin") # remove either by exact name
+# friends.remove(friends[2]) # remove my specifying element's index to be removed
+# friends.clear()
+# friends.pop() # if no argument: removes last element from list. OR, if given index, removes element with this index
+# print(friends.index("Kevin")) # returns the index if element in the list or error
+# print(friends.count("Kevin")) # return count of times the element is found in the list or 0 if not found
+# lucky_numbers.sort() # by default sorts in ascending order
+# friends.reverse() # reverse the order of elements in the list
+# friends2 = friends.copy() # creates a copy of the list
+
+print(lucky_numbers)
+print(friends)
+
+
+
+
+
+
