e945f67077d66333fa06c845db27bfbbfa90741c
watfome <watfome@gmail.com>
2020-08-20 11:17:28 +0300

    first and final

---
+++ module_file_GA.py
@@ -0,0 +1,8 @@
+beatles_list = ["John Lennon", "Mike Faraday", "Nikki Glaser", "Antonio Bertolucci"]
+n_height = 1111
+
+def detect_extension(filename):
+    extension_text = filename[filename.index(".") + 1:]
+    return extension_text
+
+
